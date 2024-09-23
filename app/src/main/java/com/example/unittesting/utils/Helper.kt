46d42af835fdbd7dec12cbf182dcc74c55a8f8a5
hasnath.chowdhury <hasnath.chowdhury@reddotdigitalit.com>
2024-09-23 17:44:39 +0600

    Instrumental Tes //wip

--- app/src/main/java/com/example/unittesting/utils/Helper.kt
+++ app/src/main/java/com/example/unittesting/utils/Helper.kt
@@ -6,4 +6,35 @@ class Helper {
     fun isEven(number: Int): Boolean {
         return (number % 2 == 0)
     }
+
+    fun validatePassword(input: String) = when {
+        input.isBlank() -> {
+            "Password is required field"
+        }
+
+        input.length < 6 -> "Length of the password should be greater than 6"
+
+        input.length > 15 -> {
+            "Length of the password should be less than 15"
+        }
+
+        else -> "Valid"
+    }
+
+    fun reverseString(input: String?): String {
+        if (input == null) {
+            throw IllegalArgumentException("Input String is Required")
+        }
+        var chars = input.toCharArray()
+        var i = 0
+        var j = chars.size - 1
+        while (i < j) {
+            val temp = chars[i]
+            chars[i] = chars[j]
+            chars[j] = temp
+            i++
+            j--
+        }
+        return chars.joinToString("")
+    }
 }
